3e2a49908a9baabbd3089d68588f1662f610ea06
Ana Tulea <anatulea14@gmail.com>
2020-10-05 21:23:18 -0700

    first pass moving_zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,10 +4,15 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
-
-    pass
-
-
+    zeros =[] 
+    non_zero =[]
+    for i in range(0, len(arr)):
+        if arr[i]== 0:
+            zeros.append(arr[i])
+        else:
+            non_zero.append(arr[i])
+    return zeros + non_zero
+    
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
