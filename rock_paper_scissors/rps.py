4541f457a7853363aa8cf5c2635882bdd6bc6004
Ana Tulea <anatulea14@gmail.com>
2020-10-07 18:45:27 -0700

    first try

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,13 +4,14 @@ import sys
 
 def rock_paper_scissors(n):
   # Your code here
+  for i in ['r','s', 'p']:
+        for j in ['r','s', 'p']:
+          print([i, j])
 
-  pass
-
-
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+rock_paper_scissors(3)
+# if __name__ == "__main__":
+  # if len(sys.argv) > 1:
+  #   num_plays = int(sys.argv[1])
+  #   print(rock_paper_scissors(num_plays))
+  # else:
+  #   print('Usage: rps.py [num_plays]')
